1376e96a1448ce296d325797147cbb466f41df36
PASIFIK <mr.pasifik@gmail.com>
2023-12-28 13:10:32 +0700

    Startu tetap gabisa

--- buld.bat
+++ buld.bat
@@ -1,5 +1,5 @@
 go get github.com/akavel/rsrc
-rsrc -manifest manifest.xml -o rsrc.syso
+rsrc -manifest def-manifest.xml -o rsrc.syso
 
 go build -ldflags="-H windowsgui"
 
@@ -21,6 +21,5 @@ set WORKING_DIRECTORY="%CD%"
 
 REM Add entry to registry with StartIn value
 reg add %KEY% /v %ENTRY_NAME% /t REG_SZ /d %EXECUTABLE_PATH% /f
-reg add %KEY% /v "StartIn" /t REG_SZ /d %WORKING_DIRECTORY% /f
 
 echo "Startup entry added successfully."
